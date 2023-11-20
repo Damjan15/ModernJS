329a2e0ab383bf4437fcdda56cf2694371ffcaf8
Damjan15 <stamenovdamjan5@gmail.com>
2023-11-20 16:45:57 +0100

    01-variables-data-types: lesson-08-math-object added

---
+++ 01-variables-data-types/08-math-object/script.js
@@ -0,0 +1,31 @@
+/**
+ * Math Object
+ */
+
+// Math.abs(): Returns the absolute value of a number
+console.log(Math.abs(-5));
+
+// Math.round(): Returns the rounded value of a number
+console.log(Math.round(5.5));
+
+// Math.ceil(): Returns the smallest integer greater than or equal to a number (rounds up)
+console.log(Math.ceil(5.5));
+
+// Math.floor(): Returns the largest integer less than or equal to a number (rounds down)
+console.log(Math.floor(5.5));
+
+// Math.sqrt(): Returns the square root of a number
+console.log(Math.sqrt(25));
+
+// Math.pow(): Returns the value of a number raised to the specified power
+console.log(Math.pow(2, 5));
+
+// Math.min(): Returns the smallest of the two numbers
+console.log(Math.min(5, 10));
+
+// Math.max(): Returns the largest of the two numbers
+console.log(Math.max(5, 10));
+
+// Math.random(): Returns a random number between 0 and 1
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1); // This returns number between 1 and 10, and rounds it down
