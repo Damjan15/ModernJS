38d7e0a55dd1f6ede8f6998dfa9b49f82523031c
Damjan15 <stamenovdamjan5@gmail.com>
2023-11-22 15:39:16 +0100

    02-arrays-and-objects: lesson 01-array-basics added

---
+++ 02-arrays-and-objects/01-array-basics/script.js
@@ -0,0 +1,21 @@
+/**
+ * Array Basics
+ */
+
+// Creating an array -> array literal (the most used way)
+const arr = [374, 755, 398, 118, 289];
+console.log(arr);
+
+// Creating an array with Array Constructor
+const arr2 = new Array(1, 2, 3, 4, 5);
+console.log(arr2);
+
+// Getting values by index
+console.log(arr[3]);
+
+// Array with different data types
+const fruit = [1, "apples", true, [2, 3, 4]];
+console.log(fruit);
+
+// Getting the length of an array using the .length property
+console.log(arr2.length);
